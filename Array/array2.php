7ee218ba14009ea138e140699a1b7d1c942a569c
k7ine <pablomengod02@gmail.com>
2024-10-30 13:00:39 +0100

    Ejercicios de array

---
+++ Array/array2.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Contar elementos en un array: Crea un array con números y cuenta cuántos elementos tiene.
+*/
+
+$num = [14, 52, 33, 14, 15];
+
+echo "El array tiene " . count($num) . "elementos";
+
+?>
---
+++ Array/array3.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Acceder a elementos específicos: Crea un array con nombres de países y muestra el primer y el último elemento.
+*/
+
+// Hay que tener en cuenta que los array por norma general comienzan por la posición 0
+
+$pais = ["España", "Alemania", "Francia", "Italia", "Portugal"];
+
+echo "El primer país del array es: " . $pais[0] . "<br>";
+
+echo "El segundo país del array es: " . $pais[4]; 
+
+?>
