ed6912061a926c7951ba1ca78925f0e3cd5771a2
k7ine <pablomengod02@gmail.com>
2024-10-30 12:10:55 +0100

    LICENSE

---
+++ Ejercicios php/Array/array1.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Crear un array simple: Crea un array que contenga los nombres de cinco frutas. Imprime cada elemento en una nueva línea.
+*/
+
+$frutas = ["Manzana", "Pera", "Melón", "Sandía", "Melocotón"];
+
+// Recorrer el array e imprimir cada elemento en una nueva línea
+
+foreach ($frutas as $fruta) {
+    
+    echo $fruta . "<br>";
+    
+}
+
+?>
